3e709e85acc15d6d47ce007750f2fe29bb66bc9c
darroyave <dannover@gmail.com>
2024-02-12 21:22:22 -0500

    commit

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -2,20 +2,20 @@ class Product {
   final int id;
   final String name;
   final double price;
-  final int? stock ;
+  final int? stock;
 
   Product({
     required this.id,
     required this.name,
     required this.price,
-     this.stock ,
+    this.stock,
   });
 
   factory Product.fromJson(Map<String, dynamic> json) {
     return Product(
       id: json['id'],
       name: json['name'],
-      price: json['price'],
+      price: double.parse(json['price'].toString()),
       stock: json['stock'],
     );
   }
