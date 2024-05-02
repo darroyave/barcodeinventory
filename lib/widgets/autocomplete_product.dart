e7544a458aa711c0e9b1fa8a631697cfc2a0cd02
darroyave <dannover@gmail.com>
2024-05-02 16:42:09 -0500

    commit

---
+++ lib/widgets/autocomplete_product.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AutoCompleteProduct extends StatelessWidget {
+  const AutoCompleteProduct({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
