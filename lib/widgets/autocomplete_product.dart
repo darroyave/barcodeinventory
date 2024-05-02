fe9e07c8bf4a239fdbdd9ea9ce82c6160a9bd85d
darroyave <dannover@gmail.com>
2024-05-02 16:53:44 -0500

    commit

--- lib/widgets/autocomplete_product.dart
+++ lib/widgets/autocomplete_product.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/product.dart';
+
 class AutoCompleteProduct extends StatefulWidget {
   const AutoCompleteProduct({super.key});
 
@@ -12,14 +14,14 @@ class AutoCompleteProduct extends StatefulWidget {
 
 class _AutoCompleteProductState extends State<AutoCompleteProduct> {
   final TextEditingController _searchController = TextEditingController();
-  List<String> _suggestions = [];
+  List<Product> _suggestions = [];
 
   void _fetchSuggestions(String query) async {
-    final response = await http
-        .get(Uri.parse('http://example.com/suggestions?query=$query'));
+    final response =
+        await http.get(Uri.parse('/api/product/autocomplete?name=$query'));
     if (response.statusCode == 200) {
       setState(() {
-        _suggestions = List<String>.from(json.decode(response.body));
+        _suggestions = List<Product>.from(json.decode(response.body));
       });
     } else {
       throw Exception('Failed to load suggestions');
@@ -43,7 +45,7 @@ class _AutoCompleteProductState extends State<AutoCompleteProduct> {
             itemCount: _suggestions.length,
             itemBuilder: (context, index) {
               return ListTile(
-                title: Text(_suggestions[index]),
+                title: Text(_suggestions[index].name),
                 onTap: () {},
               );
             },
