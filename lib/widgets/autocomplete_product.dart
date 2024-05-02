4d5c5d0781823c2cb37410dd38ae4ba4f19a8c72
darroyave <dannover@gmail.com>
2024-05-02 16:49:05 -0500

    commit

--- lib/widgets/autocomplete_product.dart
+++ lib/widgets/autocomplete_product.dart
@@ -1,10 +1,55 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class AutoCompleteProduct extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class AutoCompleteProduct extends StatefulWidget {
   const AutoCompleteProduct({super.key});
 
   @override
+  State<AutoCompleteProduct> createState() => _AutoCompleteProductState();
+}
+
+class _AutoCompleteProductState extends State<AutoCompleteProduct> {
+  final TextEditingController _searchController = TextEditingController();
+  List<String> _suggestions = [];
+
+  void _fetchSuggestions(String query) async {
+    final response = await http
+        .get(Uri.parse('http://example.com/suggestions?query=$query'));
+    if (response.statusCode == 200) {
+      setState(() {
+        _suggestions = List<String>.from(json.decode(response.body));
+      });
+    } else {
+      throw Exception('Failed to load suggestions');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        TextField(
+          controller: _searchController,
+          onChanged: _fetchSuggestions,
+          decoration: const InputDecoration(
+            labelText: 'Search',
+            hintText: 'Type something...',
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: _suggestions.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(_suggestions[index]),
+                onTap: () {},
+              );
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
